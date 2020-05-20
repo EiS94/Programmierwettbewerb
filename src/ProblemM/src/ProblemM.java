ca3643bf1b884cbae1c36ceb6c1cb95158ac4e50
Eike Schaubeck <eike.schaubeck@stud-mail.uni-wuerzburg.de>
2020-05-20 16:54:34 +0200

    ProblemM update

--- src/ProblemM/src/ProblemM.java
+++ src/ProblemM/src/ProblemM.java
@@ -2,39 +2,47 @@ package ProblemM.src;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class ProblemM {
 
     public static HashMap<Integer, Node> nodes;
 
-    public static class Node{
+    public static class Node {
 
         public int color = 0;
-        public int d = Integer.MAX_VALUE;
+        public long d = Long.MAX_VALUE;
         public LinkedList<Edge> edges = new LinkedList<>();
     }
 
-    public static class Edge{
+    public static class Edge {
 
         public int nextNode;
         public int weight;
 
-        public Edge(int nextNode, int weight){
+        public Edge(int nextNode, int weight) {
             this.nextNode = nextNode;
             this.weight = weight;
         }
+
+        @Override
+        public String toString() {
+            return "next Node: " + nextNode;
+        }
     }
 
-    public static String path = "C:\\Users\\Benedikt\\Desktop\\UNI\\Informatik\\6.Semester\\Seminar Prog\\Prog\\seminarprogproblemc\\src\\ProblemM\\samples\\sample-M.";
+    //public static String path = "/home/eike/Dokumente/Uni/6. Semester/Seminar/Git/seminarprogproblemc/src/ProblemM/samples/sample-M.2.in";
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         nodes = new HashMap<>();
 
 
         //start reading Input
-        BufferedReader reader = new BufferedReader(new FileReader(path + "1.in"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String firstLine = reader.readLine();
         String[] firstLineString = firstLine.split(" ");
@@ -62,19 +70,22 @@ public class ProblemM {
             int weight = edge[4];
 
             //right Nodes
-            if (noseA == 2){
+            if (noseA == 2) {
                 nodeA *= -1;
             }
-            if (noseB == 2){
+            if (noseB == 2) {
                 nodeB *= -1;
             }
 
             nodes.get(nodeA).edges.add(new Edge(nodeB * -1, weight));
+            nodes.get(nodeB * -1).edges.add(new Edge(nodeA, weight));
             nodes.get(nodeB).edges.add(new Edge(nodeA * -1, weight));
+            nodes.get(nodeA * -1).edges.add(new Edge(nodeB, weight));
 
         }
 
         int startNode = Integer.parseInt(reader.readLine());
+        Node endNode = nodes.get(-startNode);
         reader.close();
 
         //end reading Input
@@ -83,7 +94,17 @@ public class ProblemM {
         nodes.get(startNode).color = 1;
         nodes.get(startNode).d = 0;
 
-        LinkedList<Node> queue = new LinkedList<>();
+        Comparator<Node> c = new Comparator<Node>() {
+            @Override
+            public int compare(Node node, Node t1) {
+                if (node.d > t1.d) return 1;
+                else if (node.d == t1.d) return 0;
+                else return -1;
+            }
+        };
+
+        PriorityQueue<Node> queue = new PriorityQueue<>(c);
+
         queue.add(nodes.get(startNode));
         for (int i = 1; i < numberOfNodes; i++) {
             if (i != startNode) {
@@ -93,36 +114,34 @@ public class ProblemM {
         }
         //Queue sorted
 
-        while(!queue.isEmpty()){
-            Node u = queue.get(0);
-            for (Edge edge:u.edges){
+        while (!queue.isEmpty()) {
+            Node u = queue.poll();
+            for (Edge edge : u.edges) {
                 Node neighbour = nodes.get(edge.nextNode);
 
+                if (neighbour.d != Long.MAX_VALUE && neighbour.equals(endNode)) {
+                    System.out.print(neighbour.d);
+                    return;
+                }
+
                 //Relax
-                if (neighbour.color != 1 && neighbour.d > u.d + edge.weight){
+                if (neighbour.color != 1 && neighbour.d > u.d + edge.weight) {
                     neighbour.d = u.d + edge.weight;
 
                     //DecreaseKey
-                    int indexNeighbour = queue.indexOf(neighbour);
-                    for (int i = indexNeighbour; i >= 0; i--) {
-                        if (queue.get(i).d < neighbour.d){
-                            queue.remove(indexNeighbour);
-                            queue.add(i + 1,neighbour);
-                            break;
-                        }
-                    }
-                }
-            }
-            u.color = 1;
-            queue.remove(u);
-        }
+                    queue.add(u);
 
-        int total = nodes.get(-startNode).d;
-        if (total == Integer.MAX_VALUE){
-            System.out.print("impossible");
+                }
+                u.color = 1;
+            }
         }
-        else{
+        long total = nodes.get(-startNode).d;
+        if (total == Long.MAX_VALUE) {
+            System.out.print("impossible");
+        } else {
             System.out.print(total);
         }
     }
+
+
 }
