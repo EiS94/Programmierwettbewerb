9a114673ada070987bd199283a7b80f04bbcfccc
MoltoBene <benedikt.riegel@gmail.com>
2020-07-08 10:59:34 +0200

    ProblemN Anfang Dijkstra

--- src/ProblemN/ProblemN.java
+++ src/ProblemN/ProblemN.java
@@ -3,6 +3,7 @@ package ProblemN;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ProblemN {
 
@@ -18,20 +19,33 @@ public class ProblemN {
         //#WorkNodes
         int K = Integer.parseInt(strings[2]);
 
-        ArrayList<Integer> nodeWeight = new ArrayList<>();
+        int[] nodeWeight = new int[N];
         ArrayList<ArrayList<Edge>> edges = new ArrayList<>();
+        ArrayList<HashMap<Integer, Integer>> lowestEdge = new ArrayList<>();
+        int[] workWay = new int[K];
 
         for (int i = 0; i < N; i++) {
             edges.add(new ArrayList<>());
         }
+
+        for (int i = 0; i < N; i++) {
+            lowestEdge.add(new HashMap<>());
+        }
+
         boolean[] workNode = new boolean[N];
 
         strings = br.readLine().split(" ");
+        int tempWorkNode;
         for (int i = 0; i < K; i++) {
-            workNode[Integer.parseInt(strings[i])] = true;
+            tempWorkNode = Integer.parseInt(strings[i]);
+            workNode[tempWorkNode] = true;
+            workWay[i] = tempWorkNode;
         }
 
+
+        //start adding edges
         int a, b, c;
+        Integer weightA; //Lowest weight for edge (a, b) / (b, a)
         for (int i = 0; i < M; i++) {
             strings = br.readLine().split(" ");
             //Node a
@@ -41,9 +55,58 @@ public class ProblemN {
             //Weight of edge (a, b)
             c = Integer.parseInt(strings[2]);
 
-            edges.get(a).add(new Edge(c, b));
-            edges.get(b).add(new Edge(c, a));
+            weightA = lowestEdge.get(a).get(b);
+            //No edges added yet for edge (a, b) / (b, a)
+            if (weightA == null){
+                //for (a, b)
+                edges.get(a).add(new Edge(c, b));
+                lowestEdge.get(a).put(b, c);
+
+                //for (b, a)
+                edges.get(b).add(new Edge(c, a));
+                lowestEdge.get(b).put(a, c);
+            }
+            else{
+                //Weights are equaly min -> just add
+                if (weightA == c){
+                    //for (a, b)
+                    edges.get(a).add(new Edge(c, b));
+
+                    //for (b, a)
+                    edges.get(b).add(new Edge(c, a));
+                }
+                //Previously added edges have a higer weight -> delet edges && add new edge && update minEdge
+                else if (weightA > c){
+                    //for (a, b)
+                    edges.set(a, new ArrayList<>());
+                    edges.get(a).add(new Edge(c, b));
+                    lowestEdge.get(a).put(b, c);
+
+                    //for (b, a)
+                    edges.set(b, new ArrayList<>());
+                    edges.get(b).add(new Edge(c, a));
+                    lowestEdge.get(b).put(a, c);
+                }
+                //Previously added edges have a lower weight -> do nothing
+            }
         }
+        //end adding edges
+
+
+        //start Dijkstra
+        for (int i = 1; i < nodeWeight.length; i++) {
+            nodeWeight[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < workWay.length - 1; i++) {
+            for (Edge edge:edges.get(workWay[i])) {
+                if (edge.nextNode == workWay[i + 1]){
+                    nodeWeight[edge.nextNode] = nodeWeight[workWay[i]] + edge.weight;
+                }
+            }
+        }
+
+        //end Dijkstra
 
     }
 
